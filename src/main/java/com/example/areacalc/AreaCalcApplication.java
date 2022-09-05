5ae6e9090621681757310f9ef72c62cf73193746
Chaos2522 <Jan.fekete@gmx.at>
2022-09-05 17:58:34 +0200

    Initial commit

---
+++ src/main/java/com/example/areacalc/AreaCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.areacalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreaCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AreaCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/areacalc/AreaCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.areacalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreaCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
